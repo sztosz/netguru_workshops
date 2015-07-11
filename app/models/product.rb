a01e4db963f09a56c762b9cb4a1bba1deb38a921
Bartosz Nowak <sztosz@gmail.com>
2015-07-11 16:50:22 +0200

    Added validators and average_rating method to Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,12 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+  validates :description, :title, presence: true
+  validates :price, presence: true,
+                    format: { with: /\A\d+(?:\.\d{0,2})?\z/ },
+                    numericality: { greater_than: 0 }
+
+  def average_rating
+    reviews.map(&:rating).inject(0.0) { |sum, el| sum + el } / reviews.size
+  end
 end
