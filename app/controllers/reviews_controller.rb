09c78d4d1081d1705fac3cba6922959717718dd4
Bartosz Nowak <sztosz@gmail.com>
2015-07-11 22:27:19 +0200

    Ensured that errors shows in review form, when form is invalid

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,20 @@
 class ReviewsController < ApplicationController
 
-  expose(:review)
+  expose(:review) { Review.new }
   expose(:product)
+  expose(:category)
   before_action :authenticate_user!
 
   def edit
   end
 
   def create
-    review = Review.new(review_params)
+    self.review = Review.new(review_params)
     if review.save
       product.reviews << review
       redirect_to category_product_url(product.category, product), notice: 'Review was successfully created.'
     else
-      redirect_to(:back)
+      render action: 'create'
     end
   end
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
-  validates :content, :rating, :user, presence: true
+  validates :content, :rating, :user_id, presence: true
 end
